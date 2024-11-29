60bedb7b481490e40e2f6c73298bf7a39d095bdd
Gumas <148417921+Opmasterod@users.noreply.github.com>
2024-11-29 09:19:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ TUT_VID = os.environ.get("TUT_VID", "https://t.me/hotousebotes/5")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002386698373"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1002491351838"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002491351838"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
